543928ee37fbebeee8aa79ad9dace4d99da76ce1
rudnam <francisalbarracin2@gmail.com>
2025-01-14 10:29:20 +0800

    Set up tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Fruit Management App</h1>
+        <h1 className="text-3xl underline">Fruit Management App</h1>
       </header>
       <main>
         <FruitList />
