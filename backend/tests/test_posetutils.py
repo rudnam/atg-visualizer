6cfe920483ebdb9e0b7387736ee5385b191ac371
Harold Antonio <63497385+hcantonio1@users.noreply.github.com>
2025-02-12 15:22:42 +0800

    one test function for each function in PosetUtils

--- backend/tests/test_posetutils.py
+++ backend/tests/test_posetutils.py
@@ -1,7 +1,78 @@
 from app.posetutils import PosetUtils
 
 
-def testPosetUtils():
+def test_get_linear_extensions_from_graph():
     poset_utils = PosetUtils()
-
     pass
+
+
+def test_get_linear_extensions_from_relation():
+    pass
+
+
+def test_get_graph_from_relation():
+    pass
+
+
+def test_get_hasse_from_partial_order():
+    pass
+
+
+def test_ancestors():
+    pass
+
+
+def test_descendants():
+    pass
+
+
+def test_hasse_dist():
+    pass
+
+
+def test_covers():
+    pass
+
+
+def test_x_is_covered_by_y_in_L():
+    pass
+
+
+def test_x_is_less_than_y_in_L():
+    pass
+
+
+def test_swap_xy_in_L():
+    pass
+
+
+def test_edge_label():
+    pass
+
+
+def test_generate_convex():
+    pass
+
+
+def test_get_partial_order_of_convex():
+    pass
+
+
+def main():
+    test_get_linear_extensions_from_graph()
+    test_get_linear_extensions_from_relation()
+    test_get_graph_from_relation()
+    test_get_hasse_from_partial_order()
+    test_ancestors()
+    test_descendants()
+    test_hasse_dist()
+    test_covers()
+    test_x_is_covered_by_y_in_L()
+    test_x_is_less_than_y_in_L()
+    test_swap_xy_in_L()
+    # test_edge_label()
+    test_generate_convex()
+    test_get_partial_order_of_convex()
+
+
+main()
