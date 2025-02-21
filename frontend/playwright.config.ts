55814fd9ef054fd3e2bb9299053eb59f73a34ed2
rudnam <francisalbarracin2@gmail.com>
2025-02-21 21:46:03 +0800

    Update playwright workflow

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -71,17 +71,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: [
-    {
-      command: "npm run dev",
-      url: "http://127.0.0.1:3000",
-      reuseExistingServer: !process.env.CI,
-    },
-    // {
-    //   command: "npm run backend",
-    //   url: "http://127.0.0.1:8000",
-    //   timeout: 120 * 1000,
-    //   reuseExistingServer: !process.env.CI,
-    // },
-  ],
+  webServer: {
+    command: "npm run dev",
+    url: "http://127.0.0.1:3000",
+    reuseExistingServer: !process.env.CI,
+  },
 });
