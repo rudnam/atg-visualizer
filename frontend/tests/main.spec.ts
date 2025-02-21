1f8a2e6875d70ed7cbbca5bab0a4a18895b5600f
rudnam <francisalbarracin2@gmail.com>
2025-02-21 20:40:57 +0800

    Add playwright test for displaying poset results

--- frontend/tests/main.spec.ts
+++ frontend/tests/main.spec.ts
@@ -17,3 +17,17 @@ test("renders atg when draw button is clicked", async ({ page }) => {
 
   await expect(plotDiv).not.toBeEmpty();
 });
+
+test("renders poset results when solve button is clicked", async ({ page }) => {
+  await page.goto("./");
+
+  const yTextarea = page.getByTestId("input-y");
+
+  await expect(page.getByTestId("poset-result-component")).toHaveCount(0);
+
+  await yTextarea.fill("1234\n4321");
+
+  await page.getByTestId("solve-button").click();
+
+  await expect(page.getByTestId("poset-result-component")).toHaveCount(2);
+});
