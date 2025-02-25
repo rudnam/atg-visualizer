b79645c078f04fd4df07e7c3d982a281ef4b0e8d
Francis A. <francisalbarracin2@gmail.com>
2025-02-25 09:03:42 +0800

    Revert changes to classes.py

--- backend/app/classes.py
+++ backend/app/classes.py
@@ -1,13 +1,13 @@
 import networkx as nx
 
-type PartialOrder = list[tuple[int, int]]
-type CoverRelation = list[tuple[int, int]]
-type LinearOrder = str
-type AcyclicDiGraph = nx.DiGraph
-type HasseDiagram = nx.DiGraph
-type AnchorPair = tuple[int, int]
-type EdgeLabel = frozenset[int, int]
-type AdjacentTranspositionGraph = nx.Graph
+PartialOrder = list[tuple[int, int]]
+CoverRelation = list[tuple[int, int]]
+LinearOrder = str
+AcyclicDiGraph = nx.DiGraph
+HasseDiagram = nx.DiGraph
+AnchorPair = tuple[int, int]
+EdgeLabel = frozenset[int, int]
+AdjacentTranspositionGraph = nx.Graph
 
 # not the same as upsilon: list[LinearOrder] as LinearExtensions must correspond to a poset
-type LinearExtensions = list[LinearOrder]
+LinearExtensions = list[LinearOrder]
