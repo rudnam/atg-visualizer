b0573ebb9661f781a67955c0372dfde3388c1d38
Harold Antonio <63497385+hcantonio1@users.noreply.github.com>
2025-03-18 13:57:50 +0800

    create new endpoint /graph_from_cover_relation (#36)
    Purpose:
    Make the drawing of the LEG of a poset specified by its cover relation
    and permutation length
    
    Parameters:
    permutation_length: length of linear orders; exactly the same as 'size'
    in /graph
        cover_relation: a list of ordered pairs of the form (a,b)
    
    Caveats:
    I don't know if the cover_relation parameter of the endpoint is of the
    correct type, i.e. list[tuple[int,int]]

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,6 +12,8 @@ from app.posetvisualizer import PosetVisualizer
 from app.posetsolver import PosetSolver
 from app.posetutils import PosetUtils
 
+from app.classes import CoverRelation, LinearExtensions
+
 
 class GraphRequest(BaseModel):
     sequence: str
@@ -66,6 +68,31 @@ def get_graph(
 
         fig_data = visualizer.get_figure_data()
 
+        return JSONResponse(content=pio.to_json(fig_data))
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+@app.get("/graph_from_cover_relation", response_model=GraphData)
+def get_posetgraph(permutation_length: int, cover_relation: list[list[int]]):
+    try:
+        if permutation_length < 2 or permutation_length > PosetVisualizer.MAX_SIZE:
+            raise ValueError(
+                f"Permutation length must be between 2 and {PosetVisualizer.MAX_SIZE}."
+            )
+
+        cover_relation1: CoverRelation = [
+            (relation[0], relation[1]) for relation in cover_relation
+        ]
+
+        visualizer = PosetVisualizer(permutation_length)
+        sequence: str = "".join(map(str, range(1, permutation_length + 1)))
+        linear_extensions: LinearExtensions = (
+            PosetUtils.get_linear_extensions_from_relation(cover_relation1, sequence)
+        )
+        visualizer.select_nodes(linear_extensions)
+
+        fig_data = visualizer.get_figure_data()
         return JSONResponse(content=pio.to_json(fig_data))
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
--- backend/tests/test_posetvisualizer.py
+++ backend/tests/test_posetvisualizer.py
@@ -435,3 +435,38 @@ def test_select_and_highlight_nodes():
     verify_edges_have_the_same_color(tester, edge_13)
     verify_edges_have_the_same_color(tester, edge_23)
     verify_edges_have_different_colors(tester, *diff_swap)
+
+
+def test_hexsquare_as_selected_nodes():
+    LINEAR_EXTENSIONS = ["1234", "1243", "1324", "1342", "1423", "1432", "4123", "4132"]
+    visualizer = PosetVisualizer(4)
+    selected_nodes = LINEAR_EXTENSIONS
+    visualizer.select_nodes(selected_nodes)
+    tester = FigureTester(visualizer.get_figure_data())
+
+    # selected/highlighted nodes are part of the atg
+    # for this case, there are no "does not exist" nodes to check
+    some_other_nodes = ["4321", "4231", "2413"]
+    verify_render_type_of_nodes(tester, selected_nodes, render_type="selected")
+    verify_render_type_of_nodes(tester, some_other_nodes, render_type="other")
+
+    # selected/highlighted edges are part of the atg, other edges are "visual aids"
+    some_selected_edges = [("1234", "1243"), ("1324", "1342"), ("4123", "4132")]
+    some_other_edges = [("4321", "4231"), ("2143", "2413")]
+    some_edges_that_dont_exist = [("1432", "4123"), ("1234", "1342"), ("1342", "1243")]
+    verify_render_type_of_edges(tester, some_selected_edges, render_type="selected")
+    verify_render_type_of_edges(tester, some_other_edges, render_type="other")
+    verify_edges_do_not_exist(tester, some_edges_that_dont_exist)
+
+    # edges are colored according to swap type except highlighted edges
+    ["1234", "1243", "1324", "1342", "1423", "1432", "4123", "4132"]
+    edge_14 = [("1423", "4123"), ("1432", "4132")]
+    edge_23 = [("1234", "1324"), ("1423", "1432"), ("4123", "4132")]
+    edge_24 = [("1324", "1342"), ("1243", "1423")]
+    edge_34 = [("1234", "1243"), ("1432", "1342")]
+    diff_swap = [("1423", "4123"), ("1243", "1423")]
+    verify_edges_have_the_same_color(tester, edge_14)
+    verify_edges_have_the_same_color(tester, edge_23)
+    verify_edges_have_the_same_color(tester, edge_24)
+    verify_edges_have_the_same_color(tester, edge_34)
+    verify_edges_have_different_colors(tester, *diff_swap)
