5d8f6f0415f56bd19c6b243e4be8f1066effb179
Francis A. <francisalbarracin2@gmail.com>
2025-05-10 18:42:03 +0800

    Implement toast notifications (#47)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,10 +5,13 @@ import { MantineProvider } from "@mantine/core";
 import Header from "./components/Header/Header";
 import Content from "./components/Content/Content";
 import theme from "./theme";
+import "@mantine/notifications/styles.css";
+import { Notifications } from "@mantine/notifications";
 
 function App() {
   return (
     <MantineProvider theme={theme}>
+      <Notifications />
       <div className="App h-full flex flex-col">
         <Header />
         <main className="grow my-8">
